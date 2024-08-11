d4224c9c19440b081bee872063d3a7a1c8593a1d
juanes <67403119+juanesGarcia@users.noreply.github.com>
2024-08-10 21:57:46 -0500

    inicio de css

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,33 @@
 
-import './App.css'
+import './Style/App.css'
 
 function App() {
 
 
   return (
-    <div>
-      <h1> vamos river</h1>
-    </div>
+    <div class="container">
+    <input type="radio" name="option" id="1" />
+    <label for="1">
+      <div class="tab-name">President</div>
+      <div class="tab-content">
+        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum ducimus et quia, sed maxime reprehenderit soluta provident id voluptas laboriosam corrupti aspernatur modi dignissimos atque iure sequi at natus facere.</h1>
+      </div>
+    </label>
+    <input type="radio" name="option" id="2" />
+    <label for="2">
+      <div class="tab-name">Airport</div>
+      <div class="tab-content">
+      </div>
+    </label>
+    <input type="radio" name="option" id="3" />
+    <label for="3">
+      <div class="tab-name">TouristicAttraction</div>
+      <div class="tab-content">
+
+      </div>
+    </label>
+
+  </div>
   )
 }
 
